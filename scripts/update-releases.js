e4c06ef522bd17158b77f654ff1c7116a62c4717
zhorton34 <zhorton999@gmail.com>
2024-02-22 06:54:37 -0600

    Updated workflow

---
+++ scripts/update-releases.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const filePath = process.argv[2]; 
+const newSignature = process.argv[3];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) throw err;
+
+  let jsonData = data;
+
+  // Step 1: Replace "ccs" with "ccs-releases"
+  const ccsReplacedData = JSON.parse(jsonData.replace(/\bccs\b/g, 'ccs-releases')); 
+
+
+  // Step 2: Update signatures 
+  function updateSignatures(obj) {
+    for (const key in obj) {
+      if (key === 'signature') {
+        obj[key] = newSignature;
+      } else if (typeof obj[key] === 'object') {
+        updateSignatures(obj[key]); // Recurse into nested objects
+      }
+    }
+  }
+
+  updateSignatures(ccsReplacedData); 
+
+  fs.writeFile(filePath, JSON.stringify(ccsReplacedData, null, 2), 'utf-8', (err) => {
+    if (err) throw err;
+    console.log('File updated successfully!');
+  });
+});
